3b40de504d13e0f2d6872400b833b44ccc2ad487
lawrencedsz9 <lawrencedsouza125@gmail.com>
2024-07-15 23:49:21 +0530

    updated the recording function , video input gets saved to recordings folder

--- record.py
+++ record.py
@@ -1,24 +1,34 @@
+# record.py
+
 import cv2
 from datetime import datetime
+import os
 
 def record():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)  # Initialize video capture from the first camera
 
+    # Define codec and output video file
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(f'recordings/{datetime.now().strftime("%H-%M-%S")}.avi', fourcc,20.0,(640,480))
+    output_path = 'C:/Users/lawrence/Desktop/pythooon/visitors/recordings/'
+    output_file = f'{output_path}{datetime.now().strftime("%H-%M-%S")}.avi'
+    out = cv2.VideoWriter(output_file, fourcc, 20.0, (640, 480))
 
-    while True:
-        _, frame = cap.read()
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-        cv2.putText(frame, f'{datetime.now().strftime("%D-%H-%M-%S")}', (50,50), cv2.FONT_HERSHEY_COMPLEX,
-                        0.6, (255,255,255), 2)
+        cv2.putText(frame, f'{datetime.now().strftime("%D-%H-%M-%S")}', (50, 50), cv2.FONT_HERSHEY_COMPLEX,
+                    0.6, (255, 255, 255), 2)
 
-        out.write(frame)
-        
+        out.write(frame)  # Write the frame to the output file
 
-        cv2.imshow("esc. to stop", frame)
+        cv2.imshow("Recording (Press 'F' to stop)", frame)  # Display the frame
 
-        if cv2.waitKey(1) == 27:
-            cap.release()
-            cv2.destroyAllWindows()
-            break 
+        # Check for 'f' key press to stop recording
+        if cv2.waitKey(1) & 0xFF == ord('f'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
